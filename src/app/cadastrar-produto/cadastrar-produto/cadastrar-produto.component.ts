0c8d9c41952f7106a57fa179d7ca322343e2d74e
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-06-29 19:23:06 -0300

    :+1:

--- src/app/cadastrar-produto/cadastrar-produto/cadastrar-produto.component.ts
+++ src/app/cadastrar-produto/cadastrar-produto/cadastrar-produto.component.ts
@@ -10,24 +10,5 @@ export class CadastrarProdutoComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const html = document.querySelector('html');
-
-    if (localStorage.getItem('mode') == 'darkmode') {
-      html.classList.add('darkmode');
-    } else {
-      localStorage.setItem('mode', 'lightmode');
-    }
-  }
-
-  darkmode($event) {
-    const html = document.querySelector('html');
-    const checkbox = document.querySelector('#mode');
-
-    if (localStorage.getItem('mode') == 'lightmode') {
-      localStorage.setItem('mode', 'darkmode');
-    } else {
-      localStorage.setItem('mode', 'lightmode');
-    }
-    document.querySelector('html').classList.toggle('darkmode');
   }
 }
--- src/app/componentes-fixos/nav/nav.component.ts
+++ src/app/componentes-fixos/nav/nav.component.ts
@@ -10,6 +10,29 @@ export class NavComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const html = document.querySelector('html');
+
+    if (localStorage.getItem('mode') == 'darkmode') {
+      html.classList.add('darkmode');
+    } else {
+      localStorage.setItem('mode', 'lightmode');
+    }
+  }
+
+  darkmode($event) {
+    console.log($event)
+
+    const html = document.querySelector('html');
+    const checkbox = document.querySelector('#mode');
+
+    
+
+    if (localStorage.getItem('mode') == 'lightmode') {
+      localStorage.setItem('mode', 'darkmode');
+    } else {
+      localStorage.setItem('mode', 'lightmode');
+    }
+    document.querySelector('html').classList.toggle('darkmode');
   }
 
 }
