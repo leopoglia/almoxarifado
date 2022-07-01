d9ea06c4034b540d58a5123ca98eec0840996c56
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-06-30 21:47:12 -0300

    :+1:

--- src/app/componentes-fixos/cabecalho/cabecalho.component.ts
+++ src/app/componentes-fixos/cabecalho/cabecalho.component.ts
@@ -11,5 +11,4 @@ export class CabecalhoComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
--- src/app/visao-geral/visao-geral/visao-geral.component.ts
+++ src/app/visao-geral/visao-geral/visao-geral.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ComponentFactoryResolver } from '@angular/core';
 
 
 @Component({
@@ -8,12 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisaoGeralComponent implements OnInit {
 
-  constructor() {
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {
   }
 
   ngOnInit() {
     localStorage.setItem('atual', '1');
   }
-
-
 }
