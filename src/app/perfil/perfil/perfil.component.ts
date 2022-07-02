af2cb6fba9ac85c9854fd2ea4577113625a9700c
K43RU <92130250+K43RU@users.noreply.github.com>
2022-07-02 00:31:32 -0300

    :+1:

--- src/app/perfil/perfil/perfil.component.ts
+++ src/app/perfil/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { jsonpFactory } from '@angular/http/src/http_module';
+import { UsuarioComponent } from 'src/app/aceitar-usuarios/usuario/usuario.component';
 
 @Component({
   selector: 'app-perfil',
@@ -10,6 +12,7 @@ export class PerfilComponent implements OnInit {
   constructor() { }
 
   usuario = localStorage.getItem('usuario')
+  primeiroNome = this.usuario.split(" ", 2);
 
   ngOnInit() {
     localStorage.setItem('atual', 'perfil')
