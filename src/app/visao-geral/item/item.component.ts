7efcd9eb98c35f280e0c980a89d51d9dc88c6bcf
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-07-06 16:44:15 -0300

    :+1:

--- src/app/visao-geral/item/item.component.ts
+++ src/app/visao-geral/item/item.component.ts
@@ -18,8 +18,10 @@ export class ItemComponent implements OnInit {
 
   produtos = [
     {id: 1, imagem: "https://portstore.com.br/wp-content/uploads/2020/08/ABRA%C3%87ADEIRA_DE_NYLON_2_5X200_PRETA-removebg-preview.png", nome: "Abraçadeira grande de nylon preta", descricao: "Abraçadeira preta", quantidade: "61"},
-    {id: 2, imagem: "https://www.fibracem.com/wp-content/uploads/2020/11/0000_Abracadeira.0.png", nome: "Abraçadeira para poste", descricao: "Abraçadeiras para poste", quantidade: "153"},
-    {id: 3, imagem: "https://i.imgur.com/rOhhM3b.png", nome: "Relé", descricao: "É um interruptor eletromecânico projetado por Michael Faraday na década de 1830.", quantidade: "153"}
+    {id: 2, imagem: "https://www.fibracem.com/wp-content/uploads/2020/11/0000_Abracadeira.0.png", nome: "Abraçadeira para poste", descricao: "Abraçadeiras para poste", quantidade: "9"},
+    {id: 3, imagem: "https://www.metalmatrix.com.br/uploads/produtos/thumb_d5137d970709908df9a0fe22a57a88ee.png", nome: "Abraçadeira pequena", descricao: "Abraçadeira de plastico pequena", quantidade: "0"},
+    {id: 4, imagem: "https://s3-sa-east-1.amazonaws.com/robocore-lojavirtual/697/images/697_1_M.png?20220504161329", nome: "Adaptador para fita de led", descricao: "Adaptador para fita de led", quantidade: "1"}
+
   ]
 
   link(id){
