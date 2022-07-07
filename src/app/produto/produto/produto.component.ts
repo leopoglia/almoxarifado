0d81edd16ec3acfe5e250aa0f9ad06338edf8e7b
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-07-07 19:27:20 -0300

    :+1:

--- src/app/produto/produto/produto.component.ts
+++ src/app/produto/produto/produto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-produto',
@@ -7,12 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.idParametro = route.snapshot.paramMap.get('id');
+   }
 
   ngOnInit() {
+    for(let i = 0; i < this.produtos.length; i++){
+      if(this.produtos[i].id == this.idParametro){
+        this.idProduto = i+1;
+        this.produtoCerto[0] = this.produtos[i];
+      } 
+    }
+    console.log(this.idProduto);
     if(localStorage.getItem('menu') == 'aberto'){
       localStorage.setItem('menu', 'abrir')
     }
   }
 
+  produtoCerto:any = [];
+  produtos:any = [
+    {id: 1, imagem: "https://portstore.com.br/wp-content/uploads/2020/08/ABRA%C3%87ADEIRA_DE_NYLON_2_5X200_PRETA-removebg-preview.png", nome: "Abraçadeira grande de nylon preta", detalhes: "Abraçadeira preta", quantidade: "61"},
+    {id: 2, imagem: "https://www.fibracem.com/wp-content/uploads/2020/11/0000_Abracadeira.0.png", nome: "Abraçadeira para poste", detalhes: "Abraçadeiras para poste", quantidade: "9"},
+    {id: 3, imagem: "https://www.metalmatrix.com.br/uploads/produtos/thumb_d5137d970709908df9a0fe22a57a88ee.png", nome: "Abraçadeira pequena", detalhes: "Abraçadeira de plastico pequena", quantidade: "0"},
+    {id: 4, imagem: "https://s3-sa-east-1.amazonaws.com/robocore-lojavirtual/697/images/697_1_M.png?20220504161329", nome: "Adaptador para fita de led", detalhes: "Adaptador para fita de led", quantidade: "1"}
+  ]
+
+  idProduto;
+  idParametro;
 }
--- src/app/visao-geral/item/item.component.ts
+++ src/app/visao-geral/item/item.component.ts
@@ -17,11 +17,10 @@ export class ItemComponent implements OnInit {
   }
 
   produtos = [
-    {id: 1, imagem: "https://portstore.com.br/wp-content/uploads/2020/08/ABRA%C3%87ADEIRA_DE_NYLON_2_5X200_PRETA-removebg-preview.png", nome: "Abraçadeira grande de nylon preta", descricao: "Abraçadeira preta", quantidade: "61"},
-    {id: 2, imagem: "https://www.fibracem.com/wp-content/uploads/2020/11/0000_Abracadeira.0.png", nome: "Abraçadeira para poste", descricao: "Abraçadeiras para poste", quantidade: "9"},
-    {id: 3, imagem: "https://www.metalmatrix.com.br/uploads/produtos/thumb_d5137d970709908df9a0fe22a57a88ee.png", nome: "Abraçadeira pequena", descricao: "Abraçadeira de plastico pequena", quantidade: "0"},
-    {id: 4, imagem: "https://s3-sa-east-1.amazonaws.com/robocore-lojavirtual/697/images/697_1_M.png?20220504161329", nome: "Adaptador para fita de led", descricao: "Adaptador para fita de led", quantidade: "1"}
-
+    {id: 1, imagem: "https://portstore.com.br/wp-content/uploads/2020/08/ABRA%C3%87ADEIRA_DE_NYLON_2_5X200_PRETA-removebg-preview.png", nome: "Abraçadeira grande de nylon preta", detalhes: "Abraçadeira preta", quantidade: "61"},
+    {id: 2, imagem: "https://www.fibracem.com/wp-content/uploads/2020/11/0000_Abracadeira.0.png", nome: "Abraçadeira para poste", detalhes: "Abraçadeiras para poste", quantidade: "9"},
+    {id: 3, imagem: "https://www.metalmatrix.com.br/uploads/produtos/thumb_d5137d970709908df9a0fe22a57a88ee.png", nome: "Abraçadeira pequena", detalhes: "Abraçadeira de plastico pequena", quantidade: "0"},
+    {id: 4, imagem: "https://s3-sa-east-1.amazonaws.com/robocore-lojavirtual/697/images/697_1_M.png?20220504161329", nome: "Adaptador para fita de led", detalhes: "Adaptador para fita de led", quantidade: "1"}
   ]
 
   link(id){
