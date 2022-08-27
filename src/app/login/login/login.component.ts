a7b86075be39cc770ade6d742eb46e1e8f56bbb6
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-08-27 15:13:00 -0300

    :+1:

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -11,48 +11,47 @@ import { UsuariosService } from '../../services/usuarios.service';
 export class LoginComponent implements OnInit {
 
   constructor(private route: Router,
-              private usuarioService: UsuariosService) { }
+    private usuarioService: UsuariosService) { }
 
-  usuario:string = '';
-  senha:string = '';
-  visualizacao:boolean = false;
-  tipoSenha:string = 'password';
+  usuario: string = '';
+  senha: string = '';
+  visualizacao: boolean = false;
+  tipoSenha: string = 'password';
 
   ngOnInit() {
     localStorage.clear();
   }
 
-  entrar(){
+  entrar() {
     localStorage.setItem('usuario', this.usuario);
     localStorage.setItem('senha', this.senha);
 
 
     this.usuarioService.login(this.usuario, this.senha)
-    .then(resultado => {
-      console.log('s')
-      if(resultado.hasOwnProperty("user")){
-        this.route.navigate(['visaogeral']);
-      }else{
-        this.route.navigate(['/']);
-      }
-    })
-    .then(resolvido => {
-    }).catch(erro => {
-    })
+      .then(resultado => {
+        if (resultado.hasOwnProperty("user")) {
+          this.route.navigate(['visaogeral']);
+        } else {
+          this.route.navigate(['/']);
+        }
+      })
+      .then(resolvido => {
+      }).catch(erro => {
+      })
 
-      
+
   }
 
-  cadastrar(){
+  cadastrar() {
     this.route.navigate(['/cadastro/1'])
   }
 
-  habilitarVisualizacao(){
+  habilitarVisualizacao() {
     this.visualizacao = !this.visualizacao;
 
-    if(this.visualizacao){
+    if (this.visualizacao) {
       this.tipoSenha = 'text';
-    }else{
+    } else {
       this.tipoSenha = 'password';
     }
   }
