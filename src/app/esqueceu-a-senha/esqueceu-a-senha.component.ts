8b823b27d2fa17229acb96407f0f61b4097bdea0
K43RU <86034275+Vytor-Rosa@users.noreply.github.com>
2022-09-21 20:37:11 -0300

    :frog:

---
+++ src/app/esqueceu-a-senha/esqueceu-a-senha.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-esqueceu-a-senha',
+  templateUrl: './esqueceu-a-senha.component.html',
+  styleUrls: ['./esqueceu-a-senha.component.css']
+})
+export class EsqueceuASenhaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
