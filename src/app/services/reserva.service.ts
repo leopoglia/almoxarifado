7cd83c87329c78de70e5c5f1417706ecaf1ad68e
K43RU <86034275+Vytor-Rosa@users.noreply.github.com>
2022-10-26 19:30:10 -0300

    ok

--- src/app/services/reserva.service.ts
+++ src/app/services/reserva.service.ts
@@ -4,5 +4,20 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class UsuariosService {
-    url: string = "/api/"
+    url: string = "localhost:8080/api"
+
+    criarReserva(dataRetirada, dataDevolucao, usuario, produtos) {
+        return new Promise((resolvido, rejeitado) => {
+            fetch(this.url + '/reservas', {
+                method: 'POST',
+                body:
+                    JSON.stringify({ dataRetirada: dataRetirada, dataDevolucao: dataDevolucao, usuario: usuario, produtos: produtos }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(resultado => resultado.json())
+                .then(resolvido)
+                .catch(rejeitado);
+        })
+    }
 }
