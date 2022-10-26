cfe88afbe8a11b55e863bb356fd1ebc28ac8244e
K43RU <86034275+Vytor-Rosa@users.noreply.github.com>
2022-10-26 19:49:45 -0300

    ok

--- src/app/services/reserva.service.ts
+++ src/app/services/reserva.service.ts
@@ -20,4 +20,40 @@ export class UsuariosService {
                 .catch(rejeitado);
         })
     }
+
+    buscarReservas() {
+        return new Promise((resolvido, rejeitado) => {
+            fetch(this.url + '/reservas', {
+                method: 'GET'
+            }).then(resultado => resultado.json())
+                .then(resolvido)
+                .catch(rejeitado);
+        })
+    }
+
+    buscarReserva(codigo) {
+        return new Promise((resolvido, rejeitado) => {
+            fetch(this.url + '/reservas/' + codigo, {
+                method: 'GET'
+            }).then(resultado => resultado.json())
+                .then(resolvido)
+                .catch(rejeitado);
+        })
+    }
+
+    atualizarReserva(dataRetirada, dataDevolucao, usuario, produtos) {
+        return new Promise((resolvido, rejeitado) => {
+            fetch(this.url + '/reservas', {
+                method: 'POST',
+                body:
+                    JSON.stringify({ dataRetirada: dataRetirada, dataDevolucao: dataDevolucao, usuario: usuario, produtos: produtos }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(resultado => resultado.json())
+                .then(resolvido)
+                .catch(rejeitado);
+        })
+    }
+
 }
