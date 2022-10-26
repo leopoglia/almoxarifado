4c8720afb0a188c51ffc934534f29d75336a6aa5
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-10-26 16:36:33 -0300

    ✔

--- src/app/visao-geral/item-grid/item-grid.component.ts
+++ src/app/visao-geral/item-grid/item-grid.component.ts
@@ -1,19 +1,60 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProdutoService } from 'src/app/services/produto.service';
-
-
 
 @Component({
   selector: 'app-item-grid',
   templateUrl: './item-grid.component.html',
   styleUrls: ['./item-grid.component.css']
 })
-export class ItemGridComponent implements OnInit {
+export class ItemGridComponent implements OnInit, OnChanges {
 
   @Input() buscar: string;
+  retornoItem: boolean = false;
+
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.ordernado == false) {
+      this.produtoService.buscarProdutos()
+        .then(resultado => {
+          this.list = resultado;
+          this.produtos = this.list.list;
+          this.tamanho = this.produtos.length;
+        }).catch(erro => {
+          console.log('ERRO AO BUSCAR USUÁRIOS', erro)
+        })
+    } else {
+
+      if (changes.ordernado) {
+        if (this.produtos) {
+          this.produtos = this.produtos.sort((a, b) => (a.NOME > b.NOME) ? 1 : ((b.NOME > a.NOME) ? -1 : 0));
+        } else {
+          this.produtos = this.list.list;
+        }
+      }
+    }
+
+  }
+
+  filter() {
+
+    if (this.buscar == '') {
+      return this.produtos;
+    }
+
+    if (this.produtos.filter((item) => {
+      return item.NOME.toLowerCase().indexOf(this.buscar.toLowerCase()) > -1;
+    }).length > 0) {
+      this.retornoItem = false;
+
+      return this.produtos.filter((item) => {
+        return item.NOME.toLowerCase().indexOf(this.buscar.toLowerCase()) > -1;
+      });
+
+    } else {
+      this.retornoItem = true;
+    }
+  }
 
 
   constructor(
@@ -36,7 +77,7 @@ export class ItemGridComponent implements OnInit {
 
   @Input() ordernado: boolean;
 
-  
+
 
 
 
